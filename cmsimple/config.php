177c34bb6fc8c39611100e02dec3280b5db29168
user <user@localhost.localdomain>
2019-01-23 22:49:10 +0200

    First commit

---
+++ cmsimple/config.php
@@ -0,0 +1,29 @@
+<?php
+$cf['security']['password']="seva800413";
+$cf['security']['type']="page";
+$cf['site']['title']="ПОЛНОЦВЕТНАЯ УФ ПЕЧАТЬ НА ПЛАСТИКЕ ПЕЧАТЬ НА СТЕКЛЕ СОБСТВЕННОЕ ОБОРУДОВАНИЕ КИЕВ ";
+$cf['site']['template']="cherrydelight";
+$cf['language']['default']="ru";
+$cf['meta']['keywords']=" печать на пвх, печать борды, печать на пластике, порезка, уф печать, печать на стекле, печать на пластике киев, печать на оракале, печать фотообои,печать на МДФ, цифровая уф печать, рекламная продукция, конструкция из пластика, акрила, стопперы, топперы, гибка пластика";
+$cf['meta']['description']="печать на пвх, печать борды, печать на пластике, порезка, уф печать, печать на стекле, печать на пластике киев, печать на оракале, печать фотообои,печать на МДФ, цифровая уф печать, рекламная продукция, конструкция из пластика, акрила, стопперы, топперы, гибка пластика";
+$cf['backup']['numberoffiles']="5";
+$cf['images']['maxsize']="150000";
+$cf['downloads']['maxsize']="1000000";
+$cf['mailform']['email']="polnotsvet@gmail.com";
+$cf['editor']['height']="(screen.availHeight)-400";
+$cf['editor']['external']="";
+$cf['menu']['color']="000000";
+$cf['menu']['highlightcolor']="808080";
+$cf['menu']['levels']="3";
+$cf['menu']['levelcatch']="10";
+$cf['menu']['sdoc']="";
+$cf['menu']['legal']="CMSimple Legal Notices";
+$cf['uri']['seperator']=":";
+$cf['uri']['length']="200";
+$cf['xhtml']['endtags']="";
+$cf['xhtml']['amp']="true";
+$cf['plugins']['folder']="";
+$cf['functions']['file']="functions.php";
+$cf['scripting']['regexp']="\#CMSimple (.*?)\#";
+
+?>
